39c9e3f4296d248255409d88c480d0e8a93428f6
root <root@localhost.localdomain>
2020-01-10 10:16:07 +0100

    first commit

---
+++ www/insert.php
@@ -0,0 +1,23 @@
+ <?php
+$servername = "db";
+$username = "user";
+$password = "test";
+$dbname = "stage";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO stagiaire (nom, prenom) VALUES ('Ramadoss','Archana')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+?>
+
